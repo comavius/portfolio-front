9dfb070674cb2d55df7efce3ff8fa8f7ee4502da
comavius <comavius@proton.me>
2024-11-03 22:12:39 +0900

    🎨 json2ts is devDeps

--- generate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if ! type json2ts &> /dev/null
-then
-    echo "json2ts could not be found"
-    exit 1
-fi
-json2ts -i content-config-schema.json -o src/types/contentTypes.ts
